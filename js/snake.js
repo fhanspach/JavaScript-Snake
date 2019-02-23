98af7edb597e1c1efe1289b789b0089812d17aab
Felix Hanspach <f.hanspach@gmail.com>
2019-02-23 14:59:23 +0100

    - added down page for entwicklerheld

--- js/snake.js
+++ js/snake.js
@@ -706,9 +706,11 @@ SNAKE.Board = SNAKE.Board || (function () {
                 if (config.fullScreen) {
                     fullScreenText = "On Windows, press F11 to play in Full Screen mode.";
                 }
-                welcomeTxt.innerHTML = "JavaScript Snake<p></p>Use the <strong>arrow keys</strong> on your keyboard to play the game. " + fullScreenText + "<p></p>";
+
+
+                welcomeTxt.innerHTML = "<h2>Snake</h2>Nutze die Pfeiltasten um die Schlange zu steuern und sammle Diamanten.</p>";
                 var welcomeStart = document.createElement("button");
-                welcomeStart.appendChild(document.createTextNode("Play Game"));
+                welcomeStart.appendChild(document.createTextNode("Los geht's!"));
                 var loadGame = function () {
                     SNAKE.removeEventListener(window, "keyup", kbShortcut, false);
                     tmpElm.style.display = "none";
